519a4a0fd5bba4e12ceeb2da3ee1b7ee484fc7d7
yoon-siu <59470640+yoon-siu@users.noreply.github.com>
2020-01-17 15:00:07 +0900

    2020.01.11 숙제

---
+++ js/homework_20200111.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+//    let q_quantity = $('#quantity').val();
+    
+//    $('.send').click(function(){
+    let q_name = $('#name').val();
+    let q_address = $('#address').val();
+    let q_phone_numbr = $('#phone_number').val();
+    let q_email = $('#email').val();
+        
+    if (q_name ==''){
+        alert('Enter Orderer Name');
+        $('#name').focus();
+        return false;
+    } else if (q_address =='') {
+        alert('Enter Sending Address');
+        $('#address').focus();
+    } else if (q_phone_number =='') {
+        alert('Enter Orderer Phone Numbers');
+        $('#phone_number').focus();
+    } else (q_email =='') {
+        alert('Enter Orderer E-Mail Address');
+        $('#email').focus();
+    }
+        
+    });
+//});
+//    try {
+//        $('input[type=text]').focus(function(){
+//            $(this).css('border-color','red');
+//        });
+//        $('input[type=text]').blur(function(){
+//            $(this).css('border-color','#c0c0c0');
+//        });
+//        }
+//        catch(e){
+//            alert(e.message);
+//    }
